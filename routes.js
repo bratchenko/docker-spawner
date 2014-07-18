cddcc662a084b50d37a0908164586b1f075784b8
Danial Bratchenko <bratchenko@gmail.com>
2014-07-18 17:02:13 +0300

    image updated hook

--- routes.js
+++ routes.js
@@ -24,14 +24,35 @@ Router.map(function () {
             this.response.setHeader('Content-Type', 'application/json');
 
             if (this.request.method !== 'POST') {
-                this.response.end(JSON.stringify({
+                return this.response.end(JSON.stringify({
                     error: this.request.method + " method not supported"
                 }));
             }
 
+            var imageString = this.request.body.image;
+            if (!imageString) {
+                return this.response.end(JSON.stringify({
+                    error: "image parameter is required"
+                }));
+            }
+            var parts = imageString.split(":");
+            if (parts.length < 2) {
+                return this.response.end(JSON.stringify({
+                    error: "image should be <image name>:<tag>"
+                }));
+            }
+            var imageTag = parts.pop();
+            var imageName = parts.join(":");
+
+            var servicesToSpawn = Services.find({imageName: imageName, imageTag: imageTag});
+
+            servicesToSpawn.forEach(function(service) {
+                serviceSpawner.spawnService(service);
+            });
+
             var requestData = this.request.body;
             this.response.end(JSON.stringify({
-                message: "Got it!"
+                success: true
             }));
         }
     });
