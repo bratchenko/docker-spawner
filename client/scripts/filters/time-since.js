8662cf413f56c0ec939db2c6b90920f328b3bb1f
Danial Bratchenko <bratchenko@gmail.com>
2014-08-13 19:14:27 +0300

    time-since fix

--- client/scripts/filters/time-since.js
+++ client/scripts/filters/time-since.js
@@ -1,6 +1,10 @@
 angular.module('dockerSpawnerApp')
     .filter('timeSince', function () {
         return function (date) {
-            return window.moment(date).fromNow();
+            if (date) {
+                return window.moment(date).fromNow();
+            } else {
+                return 'never';
+            }
         };
     });
